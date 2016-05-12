0aa5f39d6e51c91e497a683dba66f51ce2db79ed
Andrea Cardy <acardy@customink.com>
2016-05-12 12:57:11 -0400

    Assignment 2.6 functionality completed. Todo: styles.

---
+++ scripts/madLibs.js
@@ -0,0 +1,16 @@
+angular.module('madLibsApp', [])
+.controller('myCtrl', ['$scope', function($scope) {
+  $scope.words = [
+    {
+      "femaleName": "",
+      "dirtyTask": "",
+      "obnoxiousCelebrity": "",
+      "jobTitle": "",
+      "celebrity": "",
+      "hugeNumber": "",
+      "tediousTask": "",
+      "uselessSkill": "",
+      "adjective": ""
+    }
+  ];
+}]);
