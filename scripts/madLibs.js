61177de6c71eed8de2bdaeb2d29a8d857d50f916
Andrea Cardy <acardy@customink.com>
2016-05-18 20:37:40 -0400

    Mad Libs refactor #2. A lot of form markup. TODO: Find a cleaner way to handle form error messaging.

--- scripts/madLibs.js
+++ scripts/madLibs.js
@@ -1,6 +1,14 @@
-angular.module('madLibsApp', [])
+angular.module('madLibsApp', ['ngMessages'])
 .controller('myCtrl', ['$scope', function($scope) {
-  $scope.gender;
+  $scope.genders = [
+    {
+      which: "male"
+    },
+    {
+      which: "female"
+    }
+  ]
+
   $scope.words = 
     {
       "name": "",
@@ -12,5 +20,22 @@ angular.module('madLibsApp', [])
       "tediousTask": "",
       "uselessSkill": "",
       "adjective": ""
+    };
+
+    $scope.origWords = angular.copy($scope.words);
+    $scope.origGenders = angular.copy($scope.genders);
+
+    $scope.submit = function(){
+      if( $scope.wordsForm.$valid ) {
+        this.validForm = true;
+      }
+    }
+ 
+    $scope.reset = function() {
+      angular.copy($scope.origWords, $scope.words);
+       angular.copy($scope.origGenders, $scope.genders);
+      $scope.wordsForm.$setPristine();
+      $scope.validForm = false;
+      $scope.gender.which = null;
     };
 }]);
